68aa5bfd9a685ad8833d276763f1f548bdecdc14
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-15 13:20:38 -0700

    build contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,40 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form action="https://formspree.io/xoqkbzvj" method="POST">
+            <div className="form-group">
+              <input
+                name="name"
+                type="text"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
