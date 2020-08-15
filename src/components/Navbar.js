a0460065e326db94df8611bb40686242529196da
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-15 04:13:04 -0700

    add sidebar functionality for small screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,15 @@ import React from "react"
 import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
-const Navbar = () => {
+const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="hay web" />
-          <button type="button" className="toggle-btn"><FaAlignRight></FaAlignRight></button>
+          <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+            <FaAlignRight></FaAlignRight>
+          </button>
         </div>
         <PageLinks styleClass="nav-links"></PageLinks>
       </div>
